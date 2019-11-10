1f569c8ef0f0fb85e45fc899e12087b76a9d729c
furuya@MBP <takashi.furuya.0@gmail.com>
2019-11-10 23:39:06 +0900

    error処理を削除

--- nams/tasks/__init__.py
+++ nams/tasks/__init__.py
@@ -23,26 +23,23 @@ def add_numbers(a, b):
 def record_stock_value_data(code):
     # stocks = Stock.objects.filter(is_trust=False)
     # for s in stocks:
-    try:
-        data = asset_scraping.kabuoji3(code)
-        if data['status']:
-            stock = Stock.objects.get(code=code)
-            for d in data['data']:
-                if StockValueData.objects.filter(stock=stock, date=d[0]).__len__() == 0:
-                    StockValueData.objects.create(
-                        stock=stock,
-                        date=d[0],
-                        val_open=d[1],
-                        val_high=d[2],
-                        val_low=d[3],
-                        val_close=d[4],
-                        turnover=d[5],
-                    )
-        logger.info('StockValueData of {} are updated'.format(stock))
-        return True
-    except Exception as e:
-        logger.error(e)
-        return False
+    data = asset_scraping.kabuoji3(code)
+    if data['status']:
+        stock = Stock.objects.get(code=code)
+        for d in data['data']:
+            if StockValueData.objects.filter(stock=stock, date=d[0]).__len__() == 0:
+                StockValueData.objects.create(
+                    stock=stock,
+                    date=d[0],
+                    val_open=d[1],
+                    val_high=d[2],
+                    val_low=d[3],
+                    val_close=d[4],
+                    turnover=d[5],
+                )
+    logger.info('StockValueData of {} are updated'.format(stock))
+    return True
+
 
 
 
