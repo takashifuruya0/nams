4d816a4df19831b9e13285ec92b2f60c3c145fe7
furuya@MBP <takashi.furuya.0@gmail.com>
2019-11-23 21:50:09 +0900

    これでどうだ

--- web/models.py
+++ web/models.py
@@ -91,23 +91,30 @@ class Entry(models.Model):
 
     def update(self, *args, **kwargs):
         self.is_closed = True if self.remaining() == 0 else False
-
-        if self.order_set.exists() and not self.order_set.count() == self.order_set.filter(stock=self.order_set.first().stock).count():
-            raise Exception('Different stocks are linked')
-        if self.remaining() < 0:
-            raise Exception('remaining should be over 0')
-        if self.is_closed and self.date_open() > self.date_close():
-            raise Exception('date_open should be earlier than date_close')
+        if self.order_set.exists():
+            # same stocks should be linked
+            if not self.order_set.count() == self.order_set.filter(stock=self.order_set.first().stock).count():
+                raise Exception('Different stocks are linked')
+            # remaining should be over 0
+            if self.remaining() < 0:
+                raise Exception('remaining should be over 0')
+            # date_open should be earlier than date_close
+            if self.is_closed and self.date_open() > self.date_close():
+                raise Exception('date_open should be earlier than date_close')
         super().update(*args, **kwargs)
 
     def save(self, *args, **kwargs):
         self.is_closed = True if self.remaining() == 0 else False
-        if self.order_set.exists() and not self.order_set.count() == self.order_set.filter(stock=self.order_set.first().stock).count():
-            raise Exception('Different stocks are linked')
-        if self.remaining() < 0:
-            raise Exception('remaining should be over 0')
-        if self.is_closed and self.date_open() > self.date_close():
-            raise Exception('date_open should be earlier than date_close')
+        if self.order_set.exists():
+            # same stocks should be linked
+            if not self.order_set.count() == self.order_set.filter(stock=self.order_set.first().stock).count():
+                raise Exception('Different stocks are linked')
+            # remaining should be over 0
+            if self.remaining() < 0:
+                raise Exception('remaining should be over 0')
+            # date_open should be earlier than date_close
+            if self.is_closed and self.date_open() > self.date_close():
+                raise Exception('date_open should be earlier than date_close')
         super().save(*args, **kwargs)
 
 
